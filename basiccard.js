c1e5a7bc6cbb7a6edafc24948e3fc8145ce7677e
Sakina Shivji <shivjisakina@gmail.com>
2017-06-21 21:33:24 -0400

    finished clozecard

--- basiccard.js
+++ basiccard.js
@@ -27,18 +27,16 @@ function basiccard (front, back) {
 
 };
 
-var question1 = new basiccard("1: What is the only American state to begin with the letter 'P'?", "Pennsylvania");
-var question2 = new basiccard("2: What is the capital city of Spain?", "Madrid");
-var question3 = new basiccard("3: What is the largest lake completely inside Minnesota?", "Red Lake");
-
-
-/*var question4 = new basiccard("4: The three primary colors are Red, ____, and Yellow", "Blue");
-var question5 = new basiccard("5: What is the only man-made landmark that you can see from the moon?", "Great Wall of China");
-var question6 = new basiccard("6: What is the largest fish in the ocean?", "A Whale Shark");
-var question7 = new basiccard("7: What country has 8/10 of the highest mountains in the world?", "Nepal");
-var question8 = new basiccard("8: Which common word changes its pronunciation when the first letter is capitalized?", "Polish");
-var question9 = new basiccard("9: Does sound travel faster through water or steel?", "Steel");
-var question10 = new basiccard("10: What is the largest planet in the solar system?", "Jupiter");*/
+var question1 = new basiccard("1: What is the only American state to begin with the letter 'P'?", "pennsylvania");
+var question2 = new basiccard("2: What is the capital city of Spain?", "madrid");
+var question3 = new basiccard("3: What is the largest lake completely inside Minnesota?", "red lake");
+var question4 = new basiccard("4: The three primary colors are Red, ____, and Yellow", "blue");
+var question5 = new basiccard("5: What is the only man-made landmark that you can see from the moon?", "great wall of china");
+var question6 = new basiccard("6: What is the largest fish in the ocean?", "whale shark");
+var question7 = new basiccard("7: What country has 8/10 of the highest mountains in the world?", "nepal");
+var question8 = new basiccard("8: Which common word changes its pronunciation when the first letter is capitalized?", "polish");
+var question9 = new basiccard("9: Does sound travel faster through water or steel?", "steel");
+var question10 = new basiccard("10: What is the largest planet in the solar system?", "jupiter");
 
 
 inquirer.prompt([{
@@ -53,24 +51,95 @@ inquirer.prompt([{
     type: 'input',
     name: "answer3",
     message: question3.front
+}, {
+    type: 'input',
+    name: "answer4",
+    message: question4.front
+}, {
+    type: 'input',
+    name: "answer5",
+    message: question5.front
+}, {
+    type: 'input',
+    name: "answer6",
+    message: question6.front
+}, {
+    type: 'input',
+    name: "answer7",
+    message: question7.front
+}, {
+    type: 'input',
+    name: "answer8",
+    message: question8.front
+}, {
+    type: 'input',
+    name: "answer9",
+    message: question9.front
+}, {
+    type: 'input',
+    name: "answer10",
+    message: question10.front
 }
 ]).then(function (inquirer) {
 
     if (inquirer.answer1 === question1.back) {
         console.log("You're right!")
     } else {
-        console.log("That's incorrect")
+        console.log(("Thats incorrect, the correct answer is", question1.back))
     }
 
     if (inquirer.answer2 === question2.back) {
         console.log("You're right!")
     } else {
-        console.log("That's incorrect")
+        console.log(("Thats incorrect, the correct answer is", question2.back))
     }
 
     if (inquirer.answer3 === question3.back) {
         console.log("You're right!")
     } else {
-        console.log("That's incorrect")
+        console.log(("Thats incorrect, the correct answer is", question3.back))
     }
+
+    if (inquirer.answer4 === question4.back) {
+        console.log("You're right!")
+    } else {
+        console.log(("Thats incorrect, the correct answer is", question4.back))
+    }
+
+    if (inquirer.answer5 === question5.back) {
+        console.log("You're right!")
+    } else {
+        console.log(("Thats incorrect, the correct answer is", question5.back))
+    }
+
+    if (inquirer.answer6 === question6.back) {
+        console.log("You're right!")
+    } else {
+        console.log(("Thats incorrect, the correct answer is", question6.back))
+    }
+
+    if (inquirer.answer7 === question7.back) {
+        console.log("You're right!")
+    } else {
+        console.log(("Thats incorrect, the correct answer is", question7.back))
+    }
+
+    if (inquirer.answer8 === question8.back) {
+        console.log("You're right!")
+    } else {
+        console.log(("Thats incorrect, the correct answer is", question8.back))
+    }
+
+    if (inquirer.answer9 === question9.back) {
+        console.log("You're right!")
+    } else {
+        console.log(("Thats incorrect, the correct answer is", question9.back))
+    }
+
+    if (inquirer.answer10 === question10.back) {
+        console.log("You're right!")
+    } else {
+        console.log(("Thats incorrect, the correct answer is", question10.back))
+    }
+
 });
--- clozecard.js
+++ clozecard.js
@@ -1,47 +1,125 @@
 var inquirer = require("inquirer");
 
-function basiccard (front, back) {
+function clozecard (clozeDeleted, partialText, fullText) {
 
-    this.front = front;
-    this.back = back
+    this.clozeDeleted = clozeDeleted;
+    this.partialText = partialText;
+    this.fullText = fullText;
 
 };
 
-var question1 = new basiccard("1: What is the only American state to begin with the letter 'P'?", "Pennsylvania");
-var question2 = new basiccard("2: What is the capital city of Spain?", "Madrid");
-var question3 = new basiccard("3: What is the largest lake completely inside Minnesota?", "Red Lake");
-
+var question1 = new clozecard("Pennsylvania", "... is the only American state to begin with the letter 'P'?", "Pennsylvania is the only American state to begin with the letter 'P'?");
+var question2 = new clozecard("Madrid", "... is the capital city of Spain?", "Madrid is the capital city of Spain?");
+var question3 = new clozecard("Red Lake", "... is the largest lake completely inside Minnesota?", "Red Lake is the largest lake completely inside Minnesota?");
+var question4 = new clozecard("blue", "The three primary colors are Red, ..., and Yellow", "The three primary colors are Red, ___, and Yellow");
+var question5 = new clozecard("great wall of china", "The ... is the only man-made landmark that you can see from the moon?", "The great wall of china is the only man-made landmark that you can see from the moon?");
+var question6 = new clozecard("whale shark", "The ... is the largest fish in the ocean?", "The whale shark is the largest fish in the ocean?");
+var question7 = new clozecard("nepal", "... has 8/10 of the highest mountains in the world?", "nepal has 8/10 of the highest mountains in the world?");
+var question8 = new clozecard("polish", "The word ... its pronunciation when the first letter is capitalized?", "The word polish its pronunciation when the first letter is capitalized?");
+var question9 = new clozecard("steel", "Sound travels fasted through ...", "Sound travels fasted through steel");
+var question10 = new clozecard("jupiter", "... is the largest planet in the solar system?", "Jupiter is the largest planet in the solar system?");
 
 inquirer.prompt([{
     type: 'input',
     name: "answer1",
-    message: question1.front
+    message: question1.partialText
 }, {
     type: 'input',
     name: "answer2",
-    message: question2.front
+    message: question2.partialText
 }, {
     type: 'input',
     name: "answer3",
-    message: question3.front
+    message: question3.partialText
+}, {
+    type: 'input',
+    name: "answer4",
+    message: question4.partialText
+}, {
+    type: 'input',
+    name: "answer5",
+    message: question5.partialText
+}, {
+    type: 'input',
+    name: "answer6",
+    message: question6.partialText
+}, {
+    type: 'input',
+    name: "answer7",
+    message: question7.partialText
+}, {
+    type: 'input',
+    name: "answer8",
+    message: question8.partialText
+}, {
+    type: 'input',
+    name: "answer9",
+    message: question9.partialText
+}, {
+    type: 'input',
+    name: "answer10",
+    message: question10.partialText
 }
 ]).then(function (inquirer) {
 
     if (inquirer.answer1 === question1.back) {
         console.log("You're right!")
     } else {
-        console.log("That's incorrect")
+        console.log(("Thats incorrect, the correct answer is", question1.clozeDeleted))
     }
 
     if (inquirer.answer2 === question2.back) {
         console.log("You're right!")
     } else {
-        console.log("That's incorrect")
+        console.log(("Thats incorrect, the correct answer is", question2.clozeDeleted))
     }
 
     if (inquirer.answer3 === question3.back) {
         console.log("You're right!")
     } else {
-        console.log("That's incorrect")
+        console.log(("Thats incorrect, the correct answer is", question3.clozeDeleted))
     }
+
+    if (inquirer.answer4 === question4.back) {
+        console.log("You're right!")
+    } else {
+        console.log(("Thats incorrect, the correct answer is", question4.clozeDeleted))
+    }
+
+    if (inquirer.answer5 === question5.back) {
+        console.log("You're right!")
+    } else {
+        console.log(("Thats incorrect, the correct answer is", question5.clozeDeleted))
+    }
+
+    if (inquirer.answer6 === question6.back) {
+        console.log("You're right!")
+    } else {
+        console.log(("Thats incorrect, the correct answer is", question6.clozeDeleted))
+    }
+
+    if (inquirer.answer7 === question7.back) {
+        console.log("You're right!")
+    } else {
+        console.log(("Thats incorrect, the correct answer is", question7.clozeDeleted))
+    }
+
+    if (inquirer.answer8 === question8.back) {
+        console.log("You're right!")
+    } else {
+        console.log(("Thats incorrect, the correct answer is", question8.clozeDeleted))
+    }
+
+    if (inquirer.answer9 === question9.back) {
+        console.log("You're right!")
+    } else {
+        console.log(("Thats incorrect, the correct answer is", question9.clozeDeleted))
+    }
+
+    if (inquirer.answer10 === question10.back) {
+        console.log("You're right!")
+    } else {
+        console.log(("Thats incorrect, the correct answer is", question10.clozeDeleted))
+    }
+
 });
